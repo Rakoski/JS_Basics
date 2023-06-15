df69cd8cf0ffda0a2fdde1c104aabc1c2c76bbc9
Rakoski <mastrakoski@gmail.com>
2023-06-15 15:34:17 -0300

    Created Simple Rock, Paper Scissors game

---
+++ Rock_Paper_Scissors/js/script.js
@@ -0,0 +1,42 @@
+let btn = document.getElementById('btn')
+let output = document.getElementById('outputtext')
+
+function getRandomWord() {
+    var words = ["rock", "paper", "scissors"];
+    var randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  }
+  
+  // Example usage
+  var randomWord = getRandomWord();
+
+  btn.addEventListener('click', function() {
+    let input1 = document.getElementById('userInput').value;
+    let input = input1.toLowerCase();
+    if(input == randomWord) {
+        output.innerHTML = "Draw!"
+
+
+    } else {
+        if(input == "paper") {
+            if(randomWord == "scissors") {
+                output.innerHTML = "You lost! The computer played " + randomWord
+            } else if(randomWord == "rock") {
+                output.innerHTML = "You won! The computer played " + randomWord 
+            }
+        } else if(input  == "rock") {
+            if(randomWord == "paper") {
+                output.innerHTML = "You lost! The computer played " + randomWord
+            } else if(randomWord == "scissors") {
+                output.innerHTML = "You won! The computer played " + randomWord 
+            }
+        } else if(input == "paper") {
+            if(randomWord == "scissors") {
+                output.innerHTML = "You lost! The computer played " + randomWord
+            } else if(randomWord == "rock") {
+                output.innerHTML = "You won! The computer played " + randomWord 
+            }
+        }
+    }
+});
+  
