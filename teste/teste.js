71e55393057d27533b5dc0f4864d03863232755e
Rakoski <mastrakoski@gmail.com>
2023-11-12 16:27:14 -0300

    checando os maiores númeoros em nodejs

--- teste/teste.js
+++ teste/teste.js
@@ -1,2 +1,40 @@
-numeroInformado = prompt.question("Informe seu número")
+const readline = require('readline');
 
+const leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let maiorNumero = 0;
+let numeroInformado;
+let contador = 0;
+
+function verificarMaiorNumero() {
+    leitor.question("Digite um número: ", function(numero) {
+        numeroInformado = parseFloat(numero);
+
+        if (isNaN(numeroInformado)) {
+            console.log("Isso não é um número válido. Tente novamente.");
+            verificarMaiorNumero();
+            return;
+        }
+
+        if (numeroInformado > maiorNumero) {
+            maiorNumero = numeroInformado;
+            console.log("Esse agora é o maior número.");
+        } else {
+            console.log("Bom número!");
+        }
+
+        contador++;
+
+        if (contador < 5) {
+            verificarMaiorNumero();
+        } else {
+            console.log("O maior número entre os informados é: " + maiorNumero);
+            leitor.close();
+        }
+    });
+}
+
+verificarMaiorNumero();
